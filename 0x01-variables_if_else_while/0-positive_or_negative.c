2de4ab9d1a30c07e7d463e8a4088ad2757e2f804
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-09-10 05:18:22 -0700

    My if, else, amd while variabl

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints a random number and states whether it is positive, zero or negative.
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
  *
- * Retrun: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -14,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
-		printf("%d is positive\n",n);
-	else if (n==0)
-		printf("%d is negative\n",n);
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
 

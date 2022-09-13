6512a81d1272c41c35ba28ee93510d43fea7699b
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-09-13 22:09:59 +0000

    Functions, nested loops in C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#inlude "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char x;
+
+	while (count++ <= 9)
+	{
+		for (x = 'a'; x <= 'z'; z++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}

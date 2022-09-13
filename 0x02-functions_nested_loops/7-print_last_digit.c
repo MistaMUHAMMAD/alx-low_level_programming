72e8ab092b3ccd8d5f13357fa658d489ad3fb465
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-09-13 23:19:40 +0000

    Functions, nested loops in C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,7 +5,7 @@
  * @n: The number in question.
  *
  * Return: Value of the last digit.
-*/
+ */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
@@ -13,7 +13,7 @@ int print_last_digit(int n)
 	if (last_digit < 0)
 		last_digit *= -1;
 
-	_putchar(last_digit + '0')
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }

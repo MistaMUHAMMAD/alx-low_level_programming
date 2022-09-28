de9f6d323087343c3f8c5b338cd6ec0d03a26e26
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-09-28 18:44:01 +0000

    Recursions in C

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,7 +18,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int start);
 void check(char *s, int start, int end, int *flag);
-int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/

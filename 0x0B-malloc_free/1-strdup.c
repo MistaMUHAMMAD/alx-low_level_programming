9e35f25c95d82ad511a940ccc23d3ba0d071b5a0
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-03 14:02:50 +0000

    malloc - allocating a block of memory in bytes at runtime

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *	     containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ *	   Otherwise - a pointer to the duplicated string.
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
+}

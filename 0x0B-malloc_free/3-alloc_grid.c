d6498f1ed365479633abf3a5a3a3a42fcdbf8a9c
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-03 22:06:51 +0000

    malloc - allocating a block of memory in bytes at runtime

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,37 +12,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array;
-	int i = 0, j;
+	int **twoD;
+	int hgt_index, wid_index;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = (int **) malloc(sizeof(int *) * height);
-	if (array != NULL)
+
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
+		return (NULL);
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		for (; i < height; i++)
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			array[i] = (int *) malloc(sizeof(int) * width);
-			if (array[i] != NULL)
-			{
-				for (j = 0; j < width; j++)
-					array[i][j] = 0;
-			}
-			else
-			{
-				while (i >= 0)
-				{
-					free(array[i]);
-					i--;
-				}
-				free(array);
-				return (NULL);
-			}
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
+			return (NULL);
 		}
-		return (array);
+	}
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-	else
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
 	}
-		return (NULL);
-	}
+
+	return (twoD);
 }

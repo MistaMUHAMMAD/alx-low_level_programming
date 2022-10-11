c29f751804f49476c0cf9e4c53a2702ba9037099
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-11 14:46:51 +0000

    common predefined macros in C Preprocessor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -17,7 +17,7 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

d5235223c9c947a9161c66782dabf72d96002e5b
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-11 09:29:00 +0000

    common predefined macros in C Preprocessor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
-#ifndef DOG_H
-#define DOG_H
-
 /**
  * Desc: Header file that defines a new type struct dog.
  */
+
+#ifndef DOG_H
+#ifndef DOG_H
 
 /**
  * struct dog - A new type describing a dog.
@@ -21,8 +21,8 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_t;
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

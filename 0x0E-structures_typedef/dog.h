dece8e71fa6f9ab580f54b80c3b447cc0985b4cb
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-11 15:27:25 +0000

    using structures and `typedef` in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,9 +6,7 @@
  *
  * @name: The name of the dog.
  * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Desc: This struct is for a dog.
+ * @owner: The owner of the dog.i
  */
 typedef struct dog
 {
@@ -17,12 +15,9 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/**
- * dog_t - Typedef for struct dog
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* DOG_H_FILE */
+#endif

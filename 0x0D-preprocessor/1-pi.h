7b41b8d24249354cdde31d587f1a2fd94dee57f3
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-11 15:15:15 +0000

    common predefined macros in C Preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -2,8 +2,8 @@
 #define PI_H
 
 /**
- * Desc: Header file that defines a macro named PI
- *	 as an abbreviation for the token 3.14159265359.
+ * PI - file that defines a macro named PI as
+ *	an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359

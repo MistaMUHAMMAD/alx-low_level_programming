104c3c3993c45e146cf29f3dcadd60812c1f74c7
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-12 07:51:39 +0000

    using structures and `typedef` in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,46 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+/**
+ * _strlen - Finds the length of a string.
+ * @str: Teh string to be measured.
+ *
+ * Return: Th length of the string.
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies the string pointed to by src, including the
+ *	      terminating null byte, to a buffer pointed to by dest.
+ *
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = "\0";
+
+	return (dest);
+}
+
 /**
  * new_dog - Creates a new dog.
  *

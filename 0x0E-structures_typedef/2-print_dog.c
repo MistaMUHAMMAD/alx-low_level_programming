ba7c29087fd22b22464f3b9f3fbdbc63c20cf0fc
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-12 07:00:08 +0000

    using structures and `typedef` in C

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,17 @@
  */
 void print_dog(struct dog *d);
 {
-	if (d == NULL)
-		return;
-
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %S\n", d->name);
 	else
-		print("Name: %s\n", d->name);
+		print("Name: (nil)\n");
+	printf("Age: %.6f\n", d->age);
 
-	if (d->age < 0)
-		printf("Age: (nil)\n");
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner),
 	else
-		printf("Age: %f\n", d->age);
-
-	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owmer);
+	}
 }

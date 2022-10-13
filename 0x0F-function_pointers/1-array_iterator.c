522515e8573706e0d4842c2d5b5e5e5c8a6fe639
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-13 13:55:46 +0000

    function pointers in C - what they hold, where they point in virtual memory, and how to use them

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,6 +6,8 @@
  * @array: array to search through
  * @size: size of array.
  * @cmp: function used to compare.
+ *
+ * Return: first index for which cmp doesn't return 0, or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {

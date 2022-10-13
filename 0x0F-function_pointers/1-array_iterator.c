f01165efe3768a6255fb41cc3915a0b479749e2a
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-13 14:23:24 +0000

    function pointers in C - what they hold, where they point in virtual memory, and how to use them

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
 	if (!array || !action)
 		return;
 
 	for (i = 0; i < size; i++)
-		action([i]);
+		action(array[i]);
 }

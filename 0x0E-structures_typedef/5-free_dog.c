ded1ba9e2b1c78cc4915684d9d900efbce9f86d8
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-13 11:32:44 +0000

    using structures and typedef in C

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,14 +4,13 @@
 /**
  * free_dog - Frees dogs.
  *
- * @d: The dog to be freed.
+ * @d: pointer to dog to be freed.
  *
  * Return: void
  */
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
 {
 	if (d)
-		return;
 	{
 		free(d->name);
 		free(d->owner);

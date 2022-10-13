b3bbb568a1827f929a472f19b7149ca20ae9a277
MUSA Muhammad <muhammad.musa4rl@gmail.com>
2022-10-13 12:14:52 +0000

    common predefined macros in C Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-/*
- * Desc: Header file that defines a function-like macro ABS(x)
- *	 that computes the absolute value of a number x
- */
-
+#ifndef ABS_H
+#define ABS_H
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
